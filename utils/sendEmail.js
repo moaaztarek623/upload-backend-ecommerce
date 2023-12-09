7063a7b6c7c0cc46de1d7630a0db964a9a3ddb99
unknown <pc@83BDC6AE140E04D.87I0Y48e>
2023-12-09 15:30:21 +0200

    backend folder

---
+++ utils/sendEmail.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+// Path options as argument like : {email address, subject, email content and others}
+const sendEmail = async (options) => {
+  //  1) Create a transporter (transporter is the service that will send email like gmail, sendGrid and Mailgun)
+  // note: Gmail is predefined service in nodemailer but mailTrap not predefined
+
+  /* Activate in gmail "less secure app" option if you use email service
+     note : if we use gmail we can only send 500 email per day */
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT, //if secure is false, it uses 587, by default, and 465 if true
+    secure: true,
+    requireTLS: true,
+    auth: {
+      user: 'moaaztarek623@gmail.com',
+      pass: 'dldlhkobxtgqwktu',
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'E-shop App <moaaztarek623@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // 3) Actually send the email
+ await transporter.sendMail(mailOptions);
+
+};
+
+module.exports = sendEmail;
